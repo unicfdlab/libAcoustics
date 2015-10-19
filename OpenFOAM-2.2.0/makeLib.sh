b11cb16bc35f9452f63b75d6f96fecf95edbbd28
matvey.kraposhin <matvey.kraposhin@sl-BIG.inicluster.ru>
2015-10-19 14:54:15 +0300

    Added first version of libAcoustics (OpenFOAM-2.2.0)

---
+++ OpenFOAM-2.2.0/makeLib.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source ./libEnv.sh
+
+THIS_DIR=`pwd`
+
+cd $THIS_DIR/FoamFourierAnalysis/$FFTW_LIB
+
+
+CFLAGS=-fPIC\\
+CXXFLAGS=-fPIC\\
+./configure --prefix=$FOAM_USER_LIBBIN/$FFTW_LIB --enable-shared
+
+make
+
+make install
+
+cd $FOAM_USER_LIBBIN
+libs=`ls $FFTW_LIB/lib/lib*.so*`
+ls $libs
+
+for lib in $libs
+do
+    ln -s $lib
+done
+
+#
+cd $THIS_DIR
+wmake libso
+
+#
+#END-OF-FILE
+#
+
